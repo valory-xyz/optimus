e6f4bbe21b5246b79ba261a63baad6ef2fb16785
David Vilela <dvilelaf@gmail.com>
2024-07-17 13:15:53 +0200

    fix: generators

--- packages/valory/skills/superagent_abci/models.py
+++ packages/valory/skills/superagent_abci/models.py
@@ -75,7 +75,7 @@ class SharedState(BaseSharedState):
 
         SuperAgentAbciApp.event_to_timeout[
             TransactionSettlementEvent.VALIDATE_TIMEOUT
-        ] = (self.context.params.round_timeout_seconds)
+        ] = self.context.params.round_timeout_seconds
 
 
 class Params(  # pylint: disable=too-many-ancestors

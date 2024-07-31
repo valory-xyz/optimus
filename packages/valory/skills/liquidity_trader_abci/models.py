5f9dcacc9a9a3f19b4df21337114ea7517eda01a
Divya-Solulab <divya.r@solulab.co>
2024-07-31 18:22:55 +0530

    fix: asset storage

--- packages/valory/skills/liquidity_trader_abci/models.py
+++ packages/valory/skills/liquidity_trader_abci/models.py
@@ -69,4 +69,5 @@ class Params(BaseParams):
         self.lifi_check_status_url = self._ensure("lifi_check_status_url", kwargs, str)
         self.slippage_for_swap = self._ensure("slippage_for_swap", kwargs, float)
         self.allowed_dexs: List[str] = self._ensure("allowed_dexs", kwargs, List[str])
+        self.manual_gas_limit: int = self._ensure("manual_gas_limit", kwargs, int)
         super().__init__(*args, **kwargs)

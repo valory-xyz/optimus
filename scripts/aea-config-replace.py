c397817468a2de65774b047559f005bf1f1b374d
Divya-Solulab <divya.r@solulab.co>
2024-10-16 11:32:15 +0530

    fix: add mode rpc url

--- scripts/aea-config-replace.py
+++ scripts/aea-config-replace.py
@@ -49,6 +49,11 @@ def main() -> None:
             config[2]["config"]["ledger_apis"]["optimism"][
                 "address"
             ] = f"${{str:{os.getenv('OPTIMISM_LEDGER_RPC')}}}"
+            
+        if os.getenv("MODE_LEDGER_RPC"):
+            config[2]["config"]["ledger_apis"]["mode"][
+                "address"
+            ] = f"${{str:{os.getenv('MODE_LEDGER_RPC')}}}"
 
         # Params
         config[5]["models"]["params"]["args"]["setup"][

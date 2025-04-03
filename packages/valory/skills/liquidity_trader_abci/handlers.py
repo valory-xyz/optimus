c991925be3319b82a04442975a31e0434aae5028
princevijay27 <princevijay2910@gmail.com>
2025-04-04 00:05:48 +0530

    Update configuration of KV store

--- packages/valory/skills/liquidity_trader_abci/handlers.py
+++ packages/valory/skills/liquidity_trader_abci/handlers.py
@@ -22,7 +22,7 @@
 from typing import Optional, cast
 
 from aea.configurations.data_types import PublicId
-
+from aea.protocols.base import Message
 
 from packages.dvilela.protocols.kv_store.message import KvStoreMessage
 from packages.valory.protocols.srr.message import SrrMessage

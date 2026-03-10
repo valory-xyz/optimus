0eec4a3c58bb7d5ddcf7f613b08a7bd8fb5d437a
Dhairya <dhairyapatel72@gmail.com>
2026-03-11 02:44:42 +0530

    Fix/eth abi version (#248)
    * chore: update eth-abi version
    
    * chore: update hashes
    
    * fix: check-agent-runner

--- check_agent_runner.py
+++ check_agent_runner.py
@@ -17,6 +17,7 @@ def main():
     # Copy env and set your variable
     env = os.environ.copy()
     env["SKILL_OPTIMUS_ABCI_MODELS_PARAMS_ARGS_STORE_PATH"] = "/tmp"
+    env["CONNECTION_KV_STORE_CONFIG_STORE_PATH"] = "/tmp"
 
     # Cross-platform handling
     system = platform.system()
